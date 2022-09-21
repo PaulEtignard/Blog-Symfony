3a8af4ccfe75872b89348a9c5be75681c3b74eb0
Paul.etignard <paul.etignard@gmail.com>
2022-09-21 08:10:02 +0200

    Ajout de Bienvenue, Bienvenue-personne et bienvenus

--- src/Controller/BienvenueController.php
+++ src/Controller/BienvenueController.php
@@ -13,4 +13,25 @@ class BienvenueController extends AbstractController
     {
         return $this->render('bienvenue/index.html.twig');
     }
+    #[Route('/bienvenue/{nom}', name: 'app_bienvenue_personne')]
+    public function bienvenuePersonne($nom): Response
+    {
+
+        return $this->render('bienvenue/bienvenue-personne.html.twig', [
+            "nom" => $nom
+        ]);
+    }
+    #[Route('/bienvenus', name: 'app_bienvenus')]
+    public function bienvenus(): Response
+    {
+        //declarer un tableau de 3 prénom
+        $noms = ["Pierre","Paul","Jaque"];
+
+        //la vue afficge la bienvenue aux 3 prénoms
+        return $this->render('bienvenue/bienvenus.html.twig',[
+            "noms" => $noms
+        ]);
+    }
 }
+
+
